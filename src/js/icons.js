09262186ea585e1fd86b14224dbbf5ec43d3ca9b
elash1312 <lasherin1312@gmail.com>
2022-05-26 06:21:18 +0000

    SimpleCounter added

---
+++ src/js/icons.js
@@ -0,0 +1,33 @@
+/* global FontAwesomeConfig */
+
+/**
+ * 1) First you have to import the @fortawesome/fontawesome library
+*/
+import fontawesome from '@fortawesome/fontawesome';
+
+
+/**
+ * 2) Then you have to import every icon that you will use
+*/
+import faClock-desk from '@fortawesome/fontawesome-free-solid/faClock-desk';
+
+
+/**
+ * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+*/
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+
+/**
+ * 4) Add the icons into the font-awesome library
+*/
+fontawesome.library.add(
+    faClock-desk
+);
+
+
+/**
+ * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+ *    <i className="fas fa-check"></i>
+*/
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,23 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "./icons.js";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="clock">
+				<i className="fa-solid fa-clock-desk"></i>
+			</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
