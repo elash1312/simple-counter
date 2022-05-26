3b8841e74a6ea3a35655c6753f446583ea5216ff
elash1312 <lasherin1312@gmail.com>
2022-05-26 07:04:07 +0000

    finished

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,33 +1,28 @@
-/* global FontAwesomeConfig */
+// /* global FontAwesomeConfig */
 
-/**
- * 1) First you have to import the @fortawesome/fontawesome library
-*/
-import fontawesome from '@fortawesome/fontawesome';
+// /**
+//  * 1) First you have to import the @fortawesome/fontawesome library
+//  */
+// import fontawesome from "@fortawesome/fontawesome";
 
+// /**
+//  * 2) Then you have to import every icon that you will use
+//  */
+// import faClock from "@fortawesome/fontawesome-free-solid/faClock";
 
-/**
- * 2) Then you have to import every icon that you will use
-*/
-import faClock-desk from '@fortawesome/fontawesome-free-solid/faClock-desk';
+// /**
+//  * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+//  */
+// fontawesome.config = {
+// 	autoReplaceSvg: "nest",
+// };
 
+// /**
+//  * 4) Add the icons into the font-awesome library
+//  */
+// fontawesome.library.add(faClock);
 
-/**
- * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
-*/
-fontawesome.config = {
-  autoReplaceSvg: 'nest'
-};
-
-/**
- * 4) Add the icons into the font-awesome library
-*/
-fontawesome.library.add(
-    faClock-desk
-);
-
-
-/**
- * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
- *    <i className="fas fa-check"></i>
-*/
+// /**
+//  * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+//  *    <i className="fas fa-check"></i>
+//  */
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 //import react into the bundle
+import PropTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -12,15 +13,41 @@ function SimpleCounter(props) {
 	return (
 		<div className="bigCounter">
 			<div className="clock">
-				<i className="fa-solid fa-clock-desk"></i>
+				<i className="fa-solid fa-clock"></i>
 			</div>
-			<div className="four">0</div>
-			<div className="three">0</div>
-			<div className="two">0</div>
-			<div className="one">0</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
 		</div>
 	);
 }
 
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
